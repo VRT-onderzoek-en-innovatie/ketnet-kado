8876553d422187f5db7e840a7e68517b11d01b33
Martijn Vandenberghe <martijnvandenberghe@gmail.com>
2013-12-09 09:29:22 +0100

    Initial Commit

---
+++ ketnetkado/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ketnetkado
+//
+//  Created by Martijn on 9/12/13.
+//  Copyright (c) 2013 VRT Onderzoek en Innovatie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
