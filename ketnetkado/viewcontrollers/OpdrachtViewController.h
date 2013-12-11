b278318488f7cf1dc7e4d0db7ee2ef53d5a4a664
Martijn Vandenberghe <martijnvandenberghe@gmail.com>
2013-12-11 14:05:13 +0100

    Added OpdrachtViewController

---
+++ ketnetkado/viewcontrollers/OpdrachtViewController.h
@@ -0,0 +1,13 @@
+//
+//  OpdrachtViewController.h
+//  ketnetkado
+//
+//  Created by Martijn on 11/12/13.
+//  Copyright (c) 2013 VRT Onderzoek en Innovatie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpdrachtViewController : UIViewController
+
+@end
---
+++ ketnetkado/viewcontrollers/OpdrachtViewController.m
@@ -0,0 +1,38 @@
+//
+//  OpdrachtViewController.m
+//  ketnetkado
+//
+//  Created by Martijn on 11/12/13.
+//  Copyright (c) 2013 VRT Onderzoek en Innovatie. All rights reserved.
+//
+
+#import "OpdrachtViewController.h"
+
+@interface OpdrachtViewController ()
+
+@end
+
+@implementation OpdrachtViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
