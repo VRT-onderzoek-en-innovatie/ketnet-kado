3d1a487a79885a2b57328165ca3e5292ad3db119
ikbenmartijn <martijnvandenberghe@gmail.com>
2013-12-12 09:11:00 +0100

    LoginManager toegevoegd De loginmanager zal de communicatie met de Ketnet-backend doen

---
+++ ketnetkado/managers/LoginManager.h
@@ -0,0 +1,13 @@
+//
+//  LoginManager.h
+//  ketnetkado
+//
+//  Created by Martijn Vandenberghe on 12/12/13.
+//  Copyright (c) 2013 VRT Onderzoek en Innovatie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LoginManager : NSObject
+
+@end
