b4135f3bd3e3c2f148ccbd17e182aaae3174cb5c
ikbenmartijn <martijnvandenberghe@gmail.com>
2013-12-12 09:10:19 +0100

    TransportManager toegevoegd De transportmanager zal de upload afhandelen en bewaken

---
+++ ketnetkado/managers/TransportManager.h
@@ -0,0 +1,13 @@
+//
+//  TransportManager.h
+//  ketnetkado
+//
+//  Created by Martijn Vandenberghe on 12/12/13.
+//  Copyright (c) 2013 VRT Onderzoek en Innovatie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TransportManager : NSObject
+
+@end
---
+++ ketnetkado/managers/TransportManager.m
@@ -0,0 +1,13 @@
+//
+//  TransportManager.m
+//  ketnetkado
+//
+//  Created by Martijn Vandenberghe on 12/12/13.
+//  Copyright (c) 2013 VRT Onderzoek en Innovatie. All rights reserved.
+//
+
+#import "TransportManager.h"
+
+@implementation TransportManager
+
+@end
