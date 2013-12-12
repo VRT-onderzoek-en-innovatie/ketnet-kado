6d8aa5d1a3c0ca01d88a076e89dd01c3b8d5366c
ikbenmartijn <martijnvandenberghe@gmail.com>
2013-12-12 13:10:43 +0100

    LoginViewController toegevoegd

---
+++ ketnetkado/viewcontrollers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  ketnetkado
+//
+//  Created by Martijn Vandenberghe on 12/12/13.
+//  Copyright (c) 2013 VRT Onderzoek en Innovatie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
