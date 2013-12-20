54a8ad4e08eb160004ac78e9fcba4b6d766b02fb
Martijn Vandenberghe <martijnvandenberghe@gmail.com>
2013-12-20 10:49:09 +0100

    UpdateManager toegevoegd

---
+++ ketnetkado/managers/UpdateManager.h
@@ -0,0 +1,13 @@
+//
+//  UpdateManager.h
+//  KetnetReporter
+//
+//  Created by Martijn on 20/12/13.
+//  Copyright (c) 2013 VRT Onderzoek en Innovatie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UpdateManager : NSObject
+
+@end
---
+++ ketnetkado/managers/UpdateManager.m
@@ -0,0 +1,13 @@
+//
+//  UpdateManager.m
+//  KetnetReporter
+//
+//  Created by Martijn on 20/12/13.
+//  Copyright (c) 2013 VRT Onderzoek en Innovatie. All rights reserved.
+//
+
+#import "UpdateManager.h"
+
+@implementation UpdateManager
+
+@end
